e4f65139162f135ab77b277b9667260c0426e936
Campbell Crowley <github@campbellcrowley.com>
2016-09-16 14:29:20 -0700

    Initial Commit

---
+++ Math Formulas/Assets/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+  public static SceneController Instance;
+  public void Awake() {
+    if (Instance == null) {
+      DontDestroyOnLoad(gameObject);
+      Instance = this;
+      Debug.Log("1");
+    } else if (Instance != this) Destroy (gameObject);
+  }
+
+  public string PreviousScene;
+
+  public void toInstructions() {
+    PreviousScene = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene("Help");
+  }
+  public void toMenu() {
+    PreviousScene = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene("Menu");
+  }
+  public void toGame() {
+    PreviousScene = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene("Game");
+  }
+}
