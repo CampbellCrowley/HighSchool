f8b16fd447604c4b9b2edbc65ea11d5221cd85d0
Campbell Crowley <github@campbellcrowley.com>
2016-09-19 14:34:37 -0700

    Persistent Data Working

--- Math Formulas/Assets/Scripts/SceneController.cs
+++ Math Formulas/Assets/Scripts/SceneController.cs
@@ -3,27 +3,37 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
-  public static SceneController Instance;
+  public SceneController Instance;
+  public static string PreviousScene;
+
   public void Awake() {
     if (Instance == null) {
       DontDestroyOnLoad(gameObject);
       Instance = this;
       Debug.Log("1");
-    } else if (Instance != this) Destroy (gameObject);
+    } else if (Instance != this) {
+      Debug.Log("2");
+      Destroy (gameObject);
+    }
   }
 
-  public string PreviousScene;
-
-  public void toInstructions() {
+  public static void toInstructions() {
     PreviousScene = SceneManager.GetActiveScene().name;
     SceneManager.LoadScene("Help");
   }
-  public void toMenu() {
+  public static void toMenu() {
     PreviousScene = SceneManager.GetActiveScene().name;
     SceneManager.LoadScene("Menu");
   }
-  public void toGame() {
+  public static void toGame() {
     PreviousScene = SceneManager.GetActiveScene().name;
     SceneManager.LoadScene("Game");
   }
+  public static void toPrevious() {
+    string next = PreviousScene;
+    PreviousScene = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene(next);
+    toMenu();
+  }
+
 }
