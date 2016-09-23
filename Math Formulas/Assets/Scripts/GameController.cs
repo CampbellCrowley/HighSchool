11df2c289890d1bb9932e8022f89b6b7150835ce
Campbell Crowley <github@campbellcrowley.com>
2016-09-22 17:49:19 -0700

    Finished V1

--- Math Formulas/Assets/Scripts/GameController.cs
+++ Math Formulas/Assets/Scripts/GameController.cs
@@ -5,82 +5,112 @@ using System.Text.RegularExpressions;
 #pragma warning disable 0168
 
 public
- class GameController : MonoBehaviour {
-  public
-   InputField input;
-  public
-   Slider sliderObject;
-  public
-   Text sliderText;
-  private
-   GameObject[] slider;
-  public
-   Text question, output, title;
-   [Range(0.0f, 10.0f)] public float tolerance = 0.1f;
-   [Range(-5000.0f, 5000.0f)] public float moveDist = 500f;
-   [Range(0.0f, 100.0f)] public float moveSpeed = 5f;
-   [Range(0.0f, 10.0f)] public float moveMultiplier = 2f;
-   [Range(0.0f, 1.0f)] public float textSpeed = 0.03f;
-  public
-   string[] RandomText = {
-       "Thinking is fun.", "You can do it!", "I believe in you!",
-       "Don't take too long on this one.", "Hmm...",
-       "I see you!\nJust kidding, I don't have eyes... :(",
-       "Failing this test means nothing.",
-       "Passing this test means everything.", "Is this even a test?",
-       "What is the meaning of life?", "This statement is false.",
-       "The Enrichment Center regrets to inform you that this next question " +
-           "is impossible. Make no attempt to solve it.",
-       "I'm making a note here: HUGE SUCCESS.",
-       "I wonder if world domination would be fun...",
-       "Take your time, I have all day.",
-       "I bet I could do this test faster than you.",
-       "Well here we are again\nIt's always such a pleasure", "42",
-       "Why is this text even here?"};
-  public
-   string[] RandomEmpty = {
-       "That's not an answer...",
-       "Are you even trying?",
-       "The answer box is feeling very empty inside...",
-       "Nothing is not an answer.",
-       "Nice try. But no.",
-       "We wont continue until you at least attempt the question.",
-       "I have all day.",
-       "I can wait.",
-       "Answer please.",
-       "Responde por favor.",
-       "I'm bilingual, emptiness is not a language I know.",
-       "Oh come on, I can't let you off the hook that easily!"};
-  public
-   string[] RandomCorrect = {
-       "Yay! Problem {0} done!",     "Booyah! {0} defeated!",
-       "Order {1} coming up!",       "Problem {0} deep fried!",
-       "{0}? Pssh, aint nothin'.",   "Problem {0}? Never heard of it.",
-       "Problem {1} is scared now.", "Problem {0}: 0, you: 1",
-       "Ohh snap!",                  "You showed #{0} who's boss!",
-       "Challenge? HA!"};
-  private
-   bool movedown = false, moveleft = false, moveright = false,
-        writingTitle = true, resetTitle = false, isSliderVisible = true;
-  private
-   string nextTitle = "Ready?\nThe game has already begun!";
-  private
-   float deltaLastTextUpdate = 0;
-  private
-   Vector3 questionStartPos, sliderTextStart;
+class GameController : MonoBehaviour {
+ public
+  InputField input;
+ public
+  Slider sliderObject;
+ public
+  Text sliderText, sliderTextMin, sliderTextMax;
+ private
+  GameObject[] slider;
+ public
+  Text InfoText;
+ public
+  Text problemNumber;
+ public
+  Text question, output, title;
+  [Range(0.0f, 10.0f)] public float tolerance = 0.05f;
+  [Range(-5000.0f, 5000.0f)] public float moveDist = 1000f;
+  [Range(0.0f, 100.0f)] public float moveSpeed = 12.2f;
+  [Range(0.0f, 10.0f)] public float moveMultiplier = 2f;
+  [Range(0.0f, 1.0f)] public float textSpeed = 0.03f;
+ public
+  string decimalInfoText = "At least one digit after the decimal is required";
+ private
+  string[] RandomText = {
+      "Thinking is fun.", "You can do it!", "I believe in you!",
+      "Don't take too long on this one.", "Hmm...",
+      "I see you!\nJust kidding, I don't have eyes... :(",
+      "Failing this test means nothing.", "Passing this test means everything.",
+      "Is this even a test?", "What is the meaning of life?",
+      "This statement is false.",
+      "The Enrichment Center regrets to inform you that this next question " +
+          "is impossible. Make no attempt to solve it.",
+      "I'm making a note here: HUGE SUCCESS.",
+      "I wonder if world domination would be fun...",
+      "Take your time, I have all day.",
+      "I bet I could do this test faster than you.",
+      "Well here we are again\nIt's always such a pleasure", "42",
+      "This test is making you smarter, I guarantee it.", "Ayy lmao",
+      "Never gonna give you up...", "Is this real life?", "PC master race.",
+      "Macintosh? more like BADintosh! Ha ha haaaa.... please laugh...",
+      "A long time ago in a galaxy far far away...", "To infinity, and beyond!",
+      "STOP... Hammer time!", "Gee wizz! You're a math wizz!",
+      "Okay, I'm pretty sure you're cheating.", "Hey! No calculators!",
+      "I think living is a myth.", "Anyone up for a game of Sportsball?",
+      "\"Everyhing on the internet is true.\"\n-Abraham Lincoln",
+      "I dream of being as cool as Google.",
+      "Yahoo? More like, YaWhat's that? I'm hilarious, I know.",
+      "Spaces of tabs.", "Vim over Emacs.", "Team 1001 111 1 is the best.",
+      "Mei is NOT bae", "Unity over Unreal",
+      "VR would be pretty cool IF I HAD EYES!", "The top is still spinning.",
+      "You're a hairy wizard.", "First is the worst.", "Second is the best.",
+      "This has taken you " +
+          "9999999999999999999999999999999999999999999999999999999999999...",
+      "I am God! Well, at least of this game.",
+      "Doctor Who is pretty cool. He's my favorite vulcan on the Millennium " +
+          "Falcon.",
+      "Imagination is the essence of discovery.",
+      "                  Hello over there.", "Why is this text even here?"};
+ private
+  string[] RandomEmpty = {
+      "That's not an answer...",
+      "Are you even trying?",
+      "The answer box is feeling very empty inside...",
+      "Nothing is not an answer.",
+      "Nice try. But no.",
+      "We wont continue until you at least attempt the question.",
+      "I have all day.",
+      "I can wait.",
+      "Answer please.",
+      "Responde por favor.",
+      "I'm bilingual, emptiness is not a language I know.",
+      "Oh come on, I can't let you off the hook that easily!"};
+ private
+  string[] RandomCorrect = {
+      "Yay! Problem {0} done!",     "Booyah! {0} defeated!",
+      "Order {1} coming up!",       "Problem {0} deep fried!",
+      "#{0}? Pssh, aint nothin'.",  "Problem {0}? Never heard of it.",
+      "Problem {1} is scared now.", "Problem {0}: 0, you: 1",
+      "Ohh snap!",                  "You showed #{0} who's boss!",
+      "Correctamundo!",             "On to the next!",
+      "#{0} --> #{1}",              "#{0} is evolving!",
+      "Problem {1} wants a fight!", "Challenge? HA!"};
+ private
+  bool movedown = false, moveleft = false, moveright = false,
+       writingTitle = true, resetTitle = false, isSliderVisible = true;
+ private
+  string nextTitle = "Ready?\nThe game has already begun!";
+ private
+  float deltaLastTextUpdate = 0;
+ private
+  Vector3 questionStartPos, sliderTextStart;
 
- public void Awake() {
+ public
+  void Awake() {
     sliderTextStart = sliderText.transform.position;
     slider = GameObject.FindGameObjectsWithTag("Slider");
-    foreach(GameObject s in slider)
+    foreach (GameObject s in slider)
       s.SetActive(false);
     isSliderVisible = false;
- }
+  }
  public
   void Start() {
     questionStartPos = question.transform.position;
     question.text = SceneController.data.Questions[SceneController.data.number];
     output.text = "";
+    InfoText.text = "";
     input.text = SceneController.data.currentInput;
     input.ActivateInputField();
   }
@@ -88,6 +118,7 @@ public
   void Update() {
     float distFromStart =
         Vector3.Distance(question.transform.position, questionStartPos);
+    problemNumber.text = "#" + (SceneController.data.number+1);
 
     if (moveleft) {
       question.transform.position = new Vector3(
@@ -95,7 +126,7 @@ public
               (moveSpeed + distFromStart * moveMultiplier) * Time.deltaTime *
                   10f,
           question.transform.position.y, question.transform.position.z);
-      if (question.transform.position.x <= questionStartPos.x-moveDist) {
+      if (question.transform.position.x <= questionStartPos.x - moveDist) {
         moveleft = false;
         moveright = false;
         movedown = true;
@@ -106,7 +137,7 @@ public
               (moveSpeed + distFromStart * moveMultiplier) * Time.deltaTime *
                   10f,
           question.transform.position.y, question.transform.position.z);
-      if (question.transform.position.x >= questionStartPos.x+moveDist) {
+      if (question.transform.position.x >= questionStartPos.x + moveDist) {
         moveright = false;
         movedown = true;
       }
@@ -116,8 +147,7 @@ public
             new Vector3(questionStartPos.x, questionStartPos.y + moveDist,
                         questionStartPos.z);
       } else {
-        if (question.transform.position.y - moveSpeed <
-            questionStartPos.y) {
+        if (question.transform.position.y - moveSpeed < questionStartPos.y) {
           question.transform.position = questionStartPos;
           movedown = false;
           input.ActivateInputField();
@@ -125,25 +155,23 @@ public
           nextTitle = RandomText[Random.Range(0, RandomText.Length)];
           writingTitle = true;
           if (!isSliderVisible &&
+              SceneController.data.number < QuestionData.Types.Length &&
               QuestionData.Types[SceneController.data.number] ==
                   QuestionData.SLIDER) {
             foreach (GameObject s in slider)
               s.SetActive(true);
             isSliderVisible = true;
-            Debug.Log("Slider Down");
           } else if (isSliderVisible &&
                      QuestionData.Types[SceneController.data.number] !=
                          QuestionData.SLIDER) {
             foreach (GameObject s in slider)
               s.SetActive(false);
             isSliderVisible = false;
-            Debug.Log("Slider Up");
           }
         } else {
-          question.transform.position =
-              new Vector3(questionStartPos.x,
-                          question.transform.position.y - moveSpeed,
-                          questionStartPos.z);
+          question.transform.position = new Vector3(
+              questionStartPos.x, question.transform.position.y - moveSpeed,
+              questionStartPos.z);
         }
       }
       if (SceneController.data.number < SceneController.data.Questions.Length) {
@@ -152,7 +180,8 @@ public
       } else {
         question.text = "";
         title.text = "All done!\nHere comes your score!";
-        resetTitle=false;writingTitle=false;
+        resetTitle = false;
+        writingTitle = false;
       }
       input.text = "";
     } else if (SceneController.data.number >=
@@ -192,15 +221,34 @@ public
         deltaLastTextUpdate = 0;
       }
     }
-    if(isSliderVisible) {
+
+    if (isSliderVisible) {
       input.gameObject.SetActive(false);
       sliderText.transform.position =
           sliderTextStart +
-          Vector3.right * (sliderObject.normalizedValue - 0.5f) * 550f;
+          Vector3.right * (sliderObject.normalizedValue - 0.5f) * 550f / 1.8f;
+      sliderTextMax.text = "|\n|\n\n" + sliderObject.maxValue;
+      sliderTextMin.text = "|\n|\n\n" + sliderObject.minValue;
       // Slider is 550 wide
-      sliderText.text = sliderObject.value+"";
+      if (moveright || moveright || movedown)
+        sliderText.text = sliderObject.value + "";
+      else
+        sliderText.text = Random.Range(-10, 11) + "";
     } else {
       input.gameObject.SetActive(true);
+    }
+
+    if (SceneController.data.number < QuestionData.Types.Length &&
+        QuestionData.Types[SceneController.data.number] ==
+            QuestionData.NUMBER_FIELD) {
+      if (float.Parse(
+              SceneController.data.Answers[SceneController.data.number]) *
+              1f % 1 !=
+          0) {
+        InfoText.text = decimalInfoText;
+      } else {
+        InfoText.text = "";
+      }
     }
   }
  public
@@ -209,48 +257,58 @@ public
   void toScore() { SceneController.toScore(); }
  public
   void OnTextUpdate() {
-    input.text = Regex.Replace(input.text, "[^0-9.+-]", "");
+    if (SceneController.data.number < QuestionData.Types.Length &&
+        QuestionData.Types[SceneController.data.number] ==
+            QuestionData.NUMBER_FIELD) {
+      input.text = Regex.Replace(input.text, "[^0-9.+-]", "");
+    }
     SceneController.data.currentInput = input.text;
   }
  public
   void checkAnswer() {
-    input.DeactivateInputField();
-    if (input.text.Length == 0 &&
-        QuestionData.Types[SceneController.data.number] ==
-            QuestionData.TEXT_FIELD) {
-      // title.text = "That's not an answer...";
-      title.text = RandomEmpty[Random.Range(0,RandomEmpty.Length)];
-      resetTitle = false;
-      writingTitle = false;
-      output.text = "Please enter an answer";
-    } else if (validateAnswer()) {
-      // title.text =
-      //     "Yay! Problem " + (SceneController.data.number + 1) + " done!";
-      title.text = RandomCorrect[Random.Range(0, RandomCorrect.Length)]
-                       .Replace("{0}", (SceneController.data.number + 1) + "")
-                       .Replace("{1}", (SceneController.data.number + 2) + "");
-      resetTitle = false;
-      writingTitle = false;
-      output.text = "Correct!";
-      moveright = true;
-      SceneController.data.correct++;
-      SceneController.data.number++;
-    } else {
-      title.text =
-          "Aww... better luck on problem " + (SceneController.data.number + 2);
-      resetTitle = false;
-      writingTitle = false;
-      output.text = "Incorrect";
-      moveleft=true;
-      SceneController.data.incorrect++;
-      SceneController.data.number++;
+    if (SceneController.data.number < QuestionData.Types.Length) {
+      input.DeactivateInputField();
+      if (input.text.Length == 0 &&
+          QuestionData.Types[SceneController.data.number] !=
+              QuestionData.SLIDER) {
+        // title.text = "That's not an answer...";
+        title.text = RandomEmpty[Random.Range(0, RandomEmpty.Length)];
+        resetTitle = false;
+        writingTitle = false;
+        output.text = "Please enter an answer";
+      } else if (validateAnswer()) {
+        // title.text =
+        //     "Yay! Problem " + (SceneController.data.number + 1) + " done!";
+        title.text =
+            RandomCorrect[Random.Range(0, RandomCorrect.Length)]
+                .Replace("{0}", (SceneController.data.number + 1) + "")
+                .Replace("{1}", (SceneController.data.number + 2) + "");
+        resetTitle = false;
+        writingTitle = false;
+        output.text = "Correct!";
+        moveright = true;
+        SceneController.data.correct++;
+        SceneController.data.number++;
+        GameObject.Find("Particle System")
+            .GetComponent<ParticleSystem>()
+            .Play();
+      } else {
+        title.text = "Aww... better luck on problem " +
+                     (SceneController.data.number + 2);
+        resetTitle = false;
+        writingTitle = false;
+        output.text = "Incorrect";
+        moveleft = true;
+        SceneController.data.incorrect++;
+        SceneController.data.number++;
+      }
     }
   }
  private
   bool validateAnswer() {
     switch (QuestionData.Types[SceneController.data.number]) {
       default:
-      case QuestionData.TEXT_FIELD:
+      case QuestionData.NUMBER_FIELD:
         // input and answer strings are identical
         if (input.text.Equals(
                 SceneController.data.Answers[SceneController.data.number])) {
@@ -279,10 +337,23 @@ public
         Debug.Log("Incorrect\nA:" + answer_ + "\nT:" + tolerance + "\nI:" +
                   input_);
         return false;
+      case QuestionData.TEXT_FIELD:
+        // input and answer strings are identical
+        if (input.text.ToLower().Equals(
+                SceneController.data.Answers[SceneController.data.number]
+                    .ToLower())) {
+          Debug.Log("Correct under condition 1B");
+          return true;
+        }
+        Debug.Log(
+            "Incorrect\nA:" +
+                SceneController.data.Answers[SceneController.data.number] +
+            "\nT:" + tolerance + "\nI:" + sliderObject.value);
+        return false;
       case QuestionData.SLIDER:
         if (SceneController.data.Answers[SceneController.data.number].Equals(
-                sliderObject.value+"")) {
-          Debug.Log("Correct under condition 1B");
+                sliderObject.value + "")) {
+          Debug.Log("Correct under condition 1C");
           return true;
         }
         Debug.Log(
@@ -293,5 +364,5 @@ public
         return false;
     }
   }
- }
+}
 
--- Math Formulas/Assets/Scripts/SceneController.cs
+++ Math Formulas/Assets/Scripts/SceneController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
@@ -8,17 +9,24 @@ class SceneController : MonoBehaviour {
   static string PreviousScene;
  public
   static QuestionData data;
+ private
+  static bool changeScene = false;
+ private
+  static string nextScene = "Menu";
+ private
+  Image overlayImage;
+ public
+  GameObject overlayPrefab;
 
  public
   static SceneController Instance;
  public
   void Awake() {
+    AudioListener.volume = 1.0f;
     if (Instance == null) {
       DontDestroyOnLoad(gameObject);
       Instance = this;
-      // Debug.Log("1");
     } else if (Instance != this) {
-      // Debug.Log("2");
       Destroy(gameObject);
     }
   }
@@ -27,37 +35,70 @@ class SceneController : MonoBehaviour {
     data = new QuestionData();
     data.processValues();
   }
+ public
+  void Update() {
+    if (overlayImage == null) {
+      newOverlay();
+    }
+    if (changeScene) {
+      AudioListener.volume -= 0.01f * Time.deltaTime * 100f;
+      overlayImage.color +=
+          new Color(0f, 0f, 0f, 0.01f) * Time.deltaTime * 100f;
+      if (AudioListener.volume <= 0 && overlayImage.color.a >= 1) {
+        changeScene = false;
+        gotoScene(nextScene);
+      }
+    } else {
+      if (overlayImage.color.a <= 0)
+        overlayImage.color =
+            new Color(overlayImage.color.r, overlayImage.color.g,
+                      overlayImage.color.b, 0f);
+      else
+        overlayImage.color -= new Color(0f, 0f, 0f, 0.01f);
+    }
+  }
 
- public static void resetGameData() {
+ private
+  void newOverlay() {
+    GameObject overlay = Instantiate(overlayPrefab) as GameObject;
+    overlay.transform.SetParent(GameObject.Find("Canvas").transform, true);
+    overlay.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+    overlayImage = overlay.GetComponent<Image>();
+  }
+ public
+  static void resetGameData() {
     data = new QuestionData();
     data.processValues();
- }
+  }
+ private
+  void gotoScene(string scenename) {
+    PreviousScene = SceneManager.GetActiveScene().name;
+    SceneManager.LoadScene(scenename);
+  }
  public
   static void toInstructions() {
-    PreviousScene = SceneManager.GetActiveScene().name;
-    SceneManager.LoadScene("Help");
+    changeScene = true;
+    nextScene = "Help";
   }
  public
   static void toMenu() {
-    PreviousScene = SceneManager.GetActiveScene().name;
-    SceneManager.LoadScene("Menu");
+    changeScene = true;
+    nextScene = "Menu";
   }
  public
   static void toGame() {
-    PreviousScene = SceneManager.GetActiveScene().name;
-    SceneManager.LoadScene("Game");
+    changeScene = true;
+    nextScene = "Game";
   }
  public
   static void toScore() {
-    PreviousScene = SceneManager.GetActiveScene().name;
-    SceneManager.LoadScene("Score");
+    changeScene = true;
+    nextScene = "Score";
   }
  public
   static void toPrevious() {
-    string next = PreviousScene;
-    PreviousScene = SceneManager.GetActiveScene().name;
-    SceneManager.LoadScene(next);
-    toMenu();
+    changeScene = true;
+    nextScene = PreviousScene;
   }
 
 }
@@ -66,7 +107,7 @@ public class QuestionData {
  private
   const float pi = 3.14f;
  public
-  const int TEXT_FIELD = 0, SLIDER = 1;
+  const int TEXT_FIELD = 0, NUMBER_FIELD = 1, SLIDER = 2;
 
  public
   int number = 0;
@@ -87,27 +128,36 @@ public class QuestionData {
       "What is the volume of a sphere with a radius of " + getNum(3) + "?",
       "What is the distance between the two poins: (" + getNum(4, 0) + ", " +
           getNum(4, 1) + ") (" + getNum(4, 2) + ", " + getNum(4, 3) + ")?",
-      "Where is " + getNum(5,0) + " on the number line?"};
+      "Where is " + getNum(5, 0) + " on the number line?",
+      "What is the derivative of " + getNum(6, 0) + "X^" + getNum(6, 1) +
+          "?\n(Answer should be given in the same format as the question)",
+      "What is " + getNum(7, 0) + " + " + getNum(7, 1) +
+          " if we are moving south at 20 miles per hour when the plane turns " +
+          "left around Jupiter and my favourite color is capricorn?",
+      "What is " + (getNum(8, 0) * getNum(8, 1)) + " degrees F in C?",
+      "What is the slope of a line with the function " + getNum(9, 0) + "Y + " +
+          getNum(9, 1) + "X = " + getNum(9, 2) + "?"};
 
- // Ensure the first value equals the number of Questions
+  // Ensure the first value equals the number of Questions
  public
-  static float[, ] values = new float[ 6, 5 ];
+  static float[, ] values = new float[ 10, 5 ];
 
- // Ensure the size equals the number of Questions
+  // Ensure the size equals the number of Questions
  public
-  string[] Answers = new string[6];
+  string[] Answers = new string[10];
 
  public
-  static int[] Types = {TEXT_FIELD, TEXT_FIELD, TEXT_FIELD,
-                        TEXT_FIELD, TEXT_FIELD, SLIDER};
+  static int[] Types = {NUMBER_FIELD, NUMBER_FIELD, NUMBER_FIELD, NUMBER_FIELD,
+                        NUMBER_FIELD, SLIDER,       TEXT_FIELD,   NUMBER_FIELD,
+                        NUMBER_FIELD, NUMBER_FIELD};
 
  private
   static float getNum(int question, int part = 0) {
-    if (Types[question] == TEXT_FIELD) {
-      int number = Random.Range(0, 11);
+    if (Types[question] == TEXT_FIELD || Types[question] == NUMBER_FIELD) {
+      int number = Random.Range(1, 16);
       values[ question, part ] = number;
       return number;
-    } else if(Types[question] == SLIDER) {
+    } else if (Types[question] == SLIDER) {
       int number = Random.Range(-10, 11);
       values[ question, part ] = number;
       return number;
@@ -137,6 +187,17 @@ public class QuestionData {
                  "";
     // Number Line
     Answers[5] = values[ 5, 0 ] + "";
+    // Derivative
+    Answers[6] =
+        (values[ 6, 0 ] * values[ 6, 1 ]) + "X^" + (values[ 6, 1 ] - 1);
+    // Simple addition
+    Answers[7] = (values[ 7, 0 ] + values[ 7, 1 ]) + "";
+    // Temperature conversion
+    Answers[8] = (((values[ 8, 0 ] * values[ 8, 1 ]) - 32f) * 5f / 9f) + "";
+    // Slope of line
+    Answers[9] = ((values[ 9, 2 ] / values[ 9, 0 ]) /
+                  (values[ 9, 2 ] / values[ 9, 1 ])) +
+                 "";
 
     Debug.Log(string.Join("\n", Answers));
     return true;
