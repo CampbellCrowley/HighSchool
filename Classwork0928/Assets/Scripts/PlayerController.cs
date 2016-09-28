76f2a011b9a28c5169a9f96caacb6668dc87cf2f
Campbell Crowley <github@campbellcrowley.com>
2016-09-28 14:34:39 -0700

    Classwork0928

---
+++ Classwork0928/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+  public Rigidbody2D rbody;
+  public float moveForce = 10;
+
+  void FixedUpdate() {
+    float hmovements = Input.GetAxis ("Horizontal");
+    float vmovements = Input.GetAxis("Vertical");
+    if((vmovements > 0.5f || Input.GetButtonDown("Jump")) && touchGround &&
+          rbody.IsTouching(
+                      GameObject.FindWithTag("Floor").GetComponent<Collider2D>()
+                          )) {
+      rbody.AddForce(new Vector2(hmovements*moveForce, 20*moveForce));
+    } else {
+      rbody.AddForce(new Vector2(hmovements*moveForce, 0));
+    }
+  }
+}
