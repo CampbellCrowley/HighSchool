30f7bb67ed07d2f7e8e48a7dddeef9b6725f71ea
Campbell Crowley <github@campbellcrowley.com>
2016-10-10 14:39:51 -0700

    Added classwork1010

---
+++ Classwork1004/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+  public Rigidbody2D rbody;
+  public float Scale = 0.35f;
+  public float moveForce = 10;
+  public float jumpVelocity = 10;
+  public GameObject MainCamera;
+  public float xCamOffset = 8f;
+  public float yCamOffset = 1.5f;
+  private bool touchingFloor = false;
+  private bool jump = false;
+  private float hmovements, vmovements;
+
+  void Start() {
+    ResetPlayer();
+  }
+  void Update() {
+    jump = Input.GetButtonDown("Jump");
+    hmovements = Input.GetAxis ("Horizontal");
+    vmovements = Input.GetAxis("Vertical");
+    if(hmovements < 0) {
+      transform.localScale = new Vector3(-Scale, Scale, Scale);
+    } else if(hmovements > 0) {
+      transform.localScale = new Vector3(Scale, Scale, Scale);
+    }
+    MainCamera.transform.position = new Vector3(transform.position.x + xCamOffset, transform.position.y + yCamOffset, -10f);
+  }
+  void FixedUpdate() {
+    foreach(GameObject each in GameObject.FindGameObjectsWithTag("Floor")) {
+      touchingFloor = rbody.IsTouching(each.GetComponent<Collider2D>());
+      if(touchingFloor) break;
+    }
+
+    if(transform.position.y < -10)
+      ResetPlayer();
+
+    if(jump && touchingFloor) {
+      rbody.velocity = new Vector2(rbody.velocity.x, jumpVelocity);
+    } else {
+      rbody.AddForce(new Vector2(hmovements*moveForce, 0));
+    }
+
+    if(rbody.velocity.x > 7) {
+      rbody.velocity = new Vector2(7, rbody.velocity.y);
+    }
+    if(rbody.velocity.y > 10) {
+      rbody.velocity = new Vector2(rbody.velocity.x, 10);
+    }
+  }
+
+  void ResetPlayer() {
+    transform.localScale = new Vector3(Scale, Scale, Scale);
+    transform.position = new Vector2(-7.5f, 2.5f);
+    rbody.velocity = new Vector2(0f,0f);
+  }
+}
---
+++ Classwork1010/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+  public Rigidbody2D rbody;
+  public float Scale = 0.25f;
+  public float moveForce = 10;
+  public float jumpVelocity = 10;
+  public GameObject MainCamera;
+  public float xCamOffset = 8f;
+  public float yCamOffset = 1.5f;
+  public float yCamTolerance = 1f;
+  private bool touchingFloor = false;
+  private bool jump = false;
+  private float hmovements, vmovements;
+
+  void Start() {
+    ResetPlayer();
+  }
+  void Update() {
+    jump = Input.GetButtonDown("Jump");
+    hmovements = Input.GetAxis ("Horizontal");
+    vmovements = Input.GetAxis("Vertical");
+    if(hmovements < 0) {
+      transform.localScale = new Vector3(-Scale, Scale, Scale);
+    } else if(hmovements > 0) {
+      transform.localScale = new Vector3(Scale, Scale, Scale);
+    }
+    float camx = transform.position.x + xCamOffset;
+    float camy = MainCamera.transform.position.y;
+    if(camy < transform.position.y + yCamOffset - yCamTolerance) {
+      camy = transform.position.y + yCamOffset - yCamTolerance;
+    } else if(camy > transform.position.y + yCamOffset + yCamTolerance) {
+      camy = transform.position.y + yCamOffset + yCamTolerance;
+    }
+    MainCamera.transform.position = new Vector3(camx, camy, -10f);
+  }
+  void FixedUpdate() {
+    foreach(GameObject each in GameObject.FindGameObjectsWithTag("Floor")) {
+      touchingFloor = rbody.IsTouching(each.GetComponent<Collider2D>());
+      if(touchingFloor) break;
+    }
+
+    if(transform.position.y < -10)
+      ResetPlayer();
+
+    if(jump && touchingFloor) {
+      rbody.velocity = new Vector2(rbody.velocity.x, jumpVelocity);
+    } else {
+      rbody.AddForce(new Vector2(hmovements*moveForce, 0));
+    }
+
+    if(rbody.velocity.x > 7) {
+      rbody.velocity = new Vector2(7, rbody.velocity.y);
+    }
+    if(rbody.velocity.y > 10) {
+      rbody.velocity = new Vector2(rbody.velocity.x, 10);
+    }
+  }
+
+  void ResetPlayer() {
+    transform.localScale = new Vector3(Scale, Scale, Scale);
+    transform.position = new Vector2(-7.5f, 2.5f);
+    rbody.velocity = new Vector2(0f,0f);
+  }
+}
