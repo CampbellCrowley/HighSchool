c60d45124e8d5714994c924f13046e93e62c8d3f
Campbell Crowley <github@campbellcrowley.com>
2016-10-11 18:24:32 -0700

    First Level Complete

--- Classwork1010/Assets/Scripts/PlayerController.cs
+++ Classwork1010/Assets/Scripts/PlayerController.cs
@@ -1,73 +1,96 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-
-public class PlayerController : MonoBehaviour {
-
-  public Rigidbody2D rbody;
-  public float Scale = 0.25f;
-  public float moveForce = 10;
-  public float jumpVelocity = 10;
-  public GameObject MainCamera;
-  public float xCamOffset = 8f;
-  public float yCamOffset = 1.5f;
-  public float yCamTolerance = 1f;
-  private bool touchingFloor = false;
-  private bool jump = false;
-  private float hmovements, vmovements;
-  private Vector2 startPos;
+public
+class PlayerController : MonoBehaviour {
+ public
+  Rigidbody2D rbody_;
+ public
+  static Rigidbody2D rbody;
+ public
+  float Scale = 0.25f;
+ public
+  float moveForce = 10;
+ public
+  float jumpVelocity = 10;
+ public
+  GameObject MainCamera;
+ public
+  float xCamOffset = 8f;
+ public
+  float yCamOffset = 1.5f;
+ public
+  float yCamTolerance = 1f;
+ public
+  GameObject door;
+ private
+  bool touchingFloor = false;
+ private
+  bool jump = false;
+ private
+  float hmovements, vmovements;
+ private
+  Vector2 startPos;
 
   void Awake() {
+    rbody = rbody_;
     startPos = transform.position;
   }
-  void Start() {
-    ResetPlayer();
-  }
+  void Start() { ResetPlayer(); }
   void Update() {
-    jump = Input.GetButtonDown("Jump");
-    hmovements = Input.GetAxis ("Horizontal");
+    hmovements = Input.GetAxis("Horizontal");
     vmovements = Input.GetAxis("Vertical");
-    if(hmovements < 0) {
+    jump = Input.GetButton("Jump") || vmovements > 0.5;
+    if (hmovements < 0) {
       transform.localScale = new Vector3(-Scale, Scale, Scale);
-    } else if(hmovements > 0) {
+    } else if (hmovements > 0) {
       transform.localScale = new Vector3(Scale, Scale, Scale);
     }
     float camx = transform.position.x + xCamOffset;
     float camy = MainCamera.transform.position.y;
-    if(camy < transform.position.y + yCamOffset - yCamTolerance) {
+    if (camy < transform.position.y + yCamOffset - yCamTolerance) {
       camy = transform.position.y + yCamOffset - yCamTolerance;
-    } else if(camy > transform.position.y + yCamOffset + yCamTolerance) {
+    } else if (camy > transform.position.y + yCamOffset + yCamTolerance) {
       camy = transform.position.y + yCamOffset + yCamTolerance;
     }
     MainCamera.transform.position = new Vector3(camx, camy, -10f);
   }
   void FixedUpdate() {
-    foreach(GameObject each in GameObject.FindGameObjectsWithTag("Floor")) {
+    foreach (GameObject each in GameObject.FindGameObjectsWithTag("Floor")) {
       touchingFloor = rbody.IsTouching(each.GetComponent<Collider2D>());
-      if(touchingFloor) break;
+      if (touchingFloor) break;
     }
 
-    if(transform.position.y < -10)
-      ResetPlayer();
+    if (transform.position.y < -10) ResetPlayer();
 
-    if(jump && touchingFloor) {
+    if (jump && touchingFloor) {
       rbody.velocity = new Vector2(rbody.velocity.x, jumpVelocity);
+      jump = false;
     } else {
-      rbody.AddForce(new Vector2(hmovements*moveForce, 0));
+      rbody.AddForce(new Vector2(hmovements * moveForce, 0));
     }
 
-    if(rbody.velocity.x > 7) {
+    /*if (Mathf.Abs(rbody.velocity.x) > 7) {
       rbody.velocity = new Vector2(7, rbody.velocity.y);
-    }
-    if(false && rbody.velocity.y > 10) {
+    }*/
+    /*if (rbody.velocity.y > 10) {
       rbody.velocity = new Vector2(rbody.velocity.x, 10);
-    }
+    }*/
   }
 
   void ResetPlayer() {
     transform.localScale = new Vector3(Scale, Scale, Scale);
     // transform.position = new Vector2(-715f, -293f);
     transform.position = startPos;
-    rbody.velocity = new Vector2(0f,0f);
+    rbody.velocity = new Vector2(0f, 0f);
   }
+
+  void OnTriggerEnter2D(Collider2D other) {
+    if (other == door.GetComponent<Collider2D>()) {
+      nextLevel();
+      Debug.Log("Next Level");
+    }
+  }
+  void nextLevel() { SceneManager.LoadScene(1); }
 }
--- Classwork1010/Assets/Scripts/PortalController.cs
+++ Classwork1010/Assets/Scripts/PortalController.cs
@@ -1,34 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PortalController : MonoBehaviour {
+public
+class PortalController : MonoBehaviour {
+ public
+  GameObject BluePortal, OrangePortal;
+ public
+  GameObject PurplePortal, GreenPortal;
+ private
+  Collider2D BlueCollider, OrangeCollider, PurpleCollider, GreenCollider;
+ private
+  bool justTeleportedToBlue = false;
+ private
+  bool justTeleportedToOrange = false;
+ private
+  bool justTeleportedToGreen = false;
+ private
+  bool justTeleportedToPurple = false;
 
-  public GameObject BluePortal, OrangePortal;
-  public GameObject PurplePortal, GreenPortal;
-  private Collider2D BlueCollider, OrangeCollider, PurpleCollider, GreenCollider;
-
-  public void Awake() {
-     BlueCollider = BluePortal.GetComponent<Collider2D>();
-     OrangeCollider = OrangePortal.GetComponent<Collider2D>();
-     PurpleCollider = PurplePortal.GetComponent<Collider2D>();
-     GreenCollider = GreenPortal.GetComponent<Collider2D>();
+ public
+  void Awake() {
+    BlueCollider = BluePortal.GetComponent<Collider2D>();
+    OrangeCollider = OrangePortal.GetComponent<Collider2D>();
+    PurpleCollider = PurplePortal.GetComponent<Collider2D>();
+    GreenCollider = GreenPortal.GetComponent<Collider2D>();
   }
 
-  public void OnTriggerEnter2D(Collider2D other) {
-    if(other == BlueCollider) {
-      gotoOrange();
-    } else if(other == OrangeCollider) {
-      gotoBlue();
+ public
+  void OnTriggerEnter2D(Collider2D other) { trigger(other); }
+ public
+  void OnTriggerStay2D(Collider2D other) { trigger(other); }
+ public
+  void OnTriggerExit2D(Collider2D other) {
+    if (other == BlueCollider) {
+      justTeleportedToBlue = false;
+    } else if (other == OrangeCollider) {
+      justTeleportedToOrange = false;
+    } else if (other == GreenCollider) {
+      justTeleportedToGreen = false;
+    } else if (other == PurpleCollider) {
+      justTeleportedToPurple = false;
     }
   }
 
-  private void gotoOrange() {
+ private
+  void trigger(Collider2D other) {
+    if (other == BlueCollider && !justTeleportedToBlue) {
+      gotoOrange();
+    } else if (other == OrangeCollider && !justTeleportedToOrange) {
+      gotoBlue();
+    } else if (other == PurpleCollider && !justTeleportedToPurple) {
+      gotoGreen();
+    } else if (other == GreenCollider && !justTeleportedToGreen) {
+      gotoPurple();
+    }
+  }
+ private
+  void gotoOrange() {
+    justTeleportedToOrange = true;
     transform.position = OrangePortal.transform.position;
-    // transform.rotation = new Vector2(OrangePortal.transform.rotation.x - BluePortal.transform.rotation.x, transform.rotation.y);
+    rotateVelocity(Quaternion.Angle(OrangePortal.transform.rotation,
+                                    BluePortal.transform.rotation));
   }
 
-  private void gotoBlue() {
+ private
+  void gotoBlue() {
+    justTeleportedToBlue = true;
     transform.position = BluePortal.transform.position;
-    Debug.Log("Angle: " + Quaternion.Angle(BluePortal.transform.rotation, OrangePortal.transform.rotation));
+    rotateVelocity(Quaternion.Angle(BluePortal.transform.rotation,
+                                    OrangePortal.transform.rotation));
   }
+
+ private
+  void gotoPurple() {
+    justTeleportedToPurple = true;
+    transform.position = PurplePortal.transform.position;
+    rotateVelocity(Quaternion.Angle(PurplePortal.transform.rotation,
+                                    GreenPortal.transform.rotation));
+  }
+
+ private
+  void gotoGreen() {
+    justTeleportedToGreen = true;
+    transform.position = GreenPortal.transform.position;
+    rotateVelocity(Quaternion.Angle(GreenPortal.transform.rotation,
+                                    PurplePortal.transform.rotation));
+  }
+
+ private
+  void rotateVelocity(float Angle) {
+    float mag = PlayerController.rbody.velocity.magnitude;
+    float angle = Mathf.Atan(PlayerController.rbody.velocity.y /
+                                    PlayerController.rbody.velocity.x) +
+                  (Angle * Mathf.PI / 180f);
+    angle *= 180f / Mathf.PI;
+    Debug.Log("Angle: " +
+              Mathf.Atan(PlayerController.rbody.velocity.y /
+                         PlayerController.rbody.velocity.x)*180f/Mathf.PI +
+              " --> " + angle);
+    Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    Vector2 direction = rotation * new Vector2(mag, 0f);
+    Vector2 velocity = direction;
+    PlayerController.rbody.velocity = velocity;
+ }
 }
