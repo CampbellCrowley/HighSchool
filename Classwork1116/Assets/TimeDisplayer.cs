bdb4bfd33882644e0bf5de60593c01f9ea28b2d1
Campbell Crowley <github@campbellcrowley.com>
2016-11-16 14:34:22 -0800

    Added moving thing

---
+++ Classwork1116/Assets/TimeDisplayer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeDisplayer : MonoBehaviour {
+
+  private GUIText text;
+  private float time = 0f;
+  private float realTimeAdjusted = 0f;
+  private float offset = 0f;
+	// Use this for initialization
+	void Start () {
+	  text = GetComponent<GUIText>();
+    offset = Time.realtimeSinceStartup;
+    realTimeAdjusted = Time.realtimeSinceStartup;
+	}
+
+	// Update is called once per frame
+	void Update () {
+    time += Time.deltaTime;
+    realTimeAdjusted += Time.deltaTime;
+	  text.text = "RealStartup: " + Time.realtimeSinceStartup +
+	              "\nRealLoad: " + Time.timeSinceLevelLoad +
+                "\nDelta: " + Time.deltaTime +
+                "\nDeltaReal: " + time +
+                "\nDeltaErrorStartup: " + (Time.realtimeSinceStartup - time) +
+                "\nDeltaErrorLoad: " + (Time.timeSinceLevelLoad - time) +
+                "\nLoadErrorStartup: " + (Time.realtimeSinceStartup - Time.timeSinceLevelLoad) +
+                "\n\nFrames: " + Time.frameCount +
+                "\nFPSReal: " + Time.frameCount / (Time.realtimeSinceStartup-offset) +
+                "\nFPSDelta: " + Time.frameCount / Time.time +
+                "\nOffset: " + offset;
+	}
+}
