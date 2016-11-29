30e7e387124c5a030e762001378244ec4c3c37e0
Campbell Crowley <github@campbellcrowley.com>
2016-11-29 14:36:36 -0800

    Classwork1128 and Classwork1129

---
+++ Classwork1128/Assets/ProjectileController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+  public float speed = 1f;
+  public float lifespan = 2f;
+  private float birthTime;
+  void Start() {
+    birthTime = Time.timeSinceLevelLoad;
+    GetComponent<Rigidbody2D>().velocity = new Vector2(speed / transform.localScale.x, 0f);
+  }
+  void Update() {
+    if(Time.timeSinceLevelLoad - birthTime > lifespan) {
+      Destroy(gameObject);
+    }
+  }
+}
---
+++ Classwork1128/Assets/ProjectileCreator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileCreator : MonoBehaviour {
+  public GameObject projectile;
+  public float size1 = 1f;
+  public float size2 = 2f;
+  public void Update() {
+    if (Input.GetButtonDown ("Fire1")) {
+    	GameObject newProjectile = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+      newProjectile.transform.localScale = new Vector2(
+            ((transform.parent.transform.localScale.x<0) ?
+                (-newProjectile.transform.localScale.x)
+                : (newProjectile.transform.localScale.x)),
+             newProjectile.transform.localScale.y) * size1;
+    }
+    if (Input.GetButtonDown ("Fire2")) {
+    	GameObject newProjectile = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+      newProjectile.transform.localScale = new Vector2(
+            ((transform.parent.transform.localScale.x<0) ?
+                (-newProjectile.transform.localScale.x)
+                : (newProjectile.transform.localScale.x)),
+             newProjectile.transform.localScale.y) * size2;
+    }
+  }
+
+}
