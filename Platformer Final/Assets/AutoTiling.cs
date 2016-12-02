b3b6fcf3a9094ebbc3b5144fc8c09f90aaebd2af
Campbell Crowley <github@campbellcrowley.com>
2016-12-02 14:34:20 -0800

    Classwork1202

---
+++ Platformer Final/Assets/AutoTiling.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class AutoTiling : MonoBehaviour {
+  public GameObject[] tiles = new GameObject[0];
+  public void OnGUI() {
+    Event e = Event.current;
+    Debug.Log(e.control + " " + e.keyCode);
+    if(e.control && e.keyCode == KeyCode.T) {
+      Debug.Log("Tiling!");
+      for(int i=0; i<tiles.Length; i++) {
+        tiles[i].transform.position = tiles[i].transform.localScale.x*Vector2.right*i;
+      }
+    }
+  }
+}
