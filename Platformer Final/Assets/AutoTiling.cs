c338437e0c03381c45bacd8ca462276101b4f713
Campbell Crowley <github@campbellcrowley.com>
2016-12-05 14:34:10 -0800

    Added auto-tiling

--- Platformer Final/Assets/AutoTiling.cs
+++ Platformer Final/Assets/AutoTiling.cs
@@ -4,13 +4,30 @@ using System.Collections;
 [ExecuteInEditMode]
 public class AutoTiling : MonoBehaviour {
   public GameObject[] tiles = new GameObject[0];
+  public float multiplier = 1f;
+  public bool updateList = false;
+  public void UpdateList() {
+    tiles = GameObject.FindGameObjectsWithTag("FloorTile");
+  }
   public void OnGUI() {
     Event e = Event.current;
-    Debug.Log(e.control + " " + e.keyCode);
-    if(e.control && e.keyCode == KeyCode.T) {
-      Debug.Log("Tiling!");
-      for(int i=0; i<tiles.Length; i++) {
-        tiles[i].transform.position = tiles[i].transform.localScale.x*Vector2.right*i;
+    if(updateList) {
+      updateList=false;
+      UpdateList();
+    }
+    if (e.ToString() != "Repaint" && e.ToString() != "Layout") {
+      // Debug.Log("START");
+      // Debug.Log("Char " + e.character);
+      // Debug.Log("isKey " + e.isKey);
+      // Debug.Log("keyCode " + e.keyCode);
+      // Debug.Log("Mbutton " + e.button);
+      // Debug.Log("E " + e);
+      // Debug.Log("END");
+      if(e.keyCode == KeyCode.T) {
+        Debug.Log("Tiling!");
+        for(int i=0; i<tiles.Length; i++) {
+          tiles[i].transform.position = Vector2.right*i*multiplier;
+        }
       }
     }
   }
