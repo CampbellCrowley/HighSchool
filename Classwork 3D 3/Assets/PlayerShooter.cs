4d5332b4b69cf9d77add5417a596814fb09f76fd
Campbell Crowley <github@campbellcrowley.com>
2017-03-03 16:39:10 -0800

    Added projectiles properly

--- Classwork 3D 3/Assets/PlayerShooter.cs
+++ Classwork 3D 3/Assets/PlayerShooter.cs
@@ -3,14 +3,26 @@ using UnityEngine;
 public class PlayerShooter : MonoBehaviour {
   public Projectile projectilePlaceholder;
   public float projectile_speed;
+  private float lastShotTime = 0;
+
+  public void Start() {
+    projectilePlaceholder.placeholder = true;
+    lastShotTime = Time.time;
+  }
 
   public void Update() {
     float shoot = Input.GetAxis("Fire1");
-    projectilePlaceholder.lifespan = 60.0f;
-    if(shoot > 0.5f) {
-      Projectile projectile = Instantiate(projectilePlaceholder, projectilePlaceholder.transform, true);
-      projectile.GetComponent<Rigidbody> ().AddRelativeForce (Vector3.forward * projectile_speed);
+    if(shoot > 0.5f && Time.time - lastShotTime > 0.0f) {
+      lastShotTime = Time.time;
+      Projectile projectile = Instantiate(
+          projectilePlaceholder, projectilePlaceholder.transform.position,
+          projectilePlaceholder.transform.rotation, null);
+      projectile.GetComponent<Rigidbody>().AddForce(Vector3.forward *
+                                                    projectile_speed);
       projectile.lifespan = 3.0f;
+      projectile.placeholder = false;
+      Physics.IgnoreCollision(GetComponent<CapsuleCollider>(),
+                              projectile.GetComponent<CapsuleCollider>());
     }
   }
 }
--- Classwork 3D 3/Assets/Projectile.cs
+++ Classwork 3D 3/Assets/Projectile.cs
@@ -2,13 +2,23 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
   public float lifespan = 10000.0f;
+  public bool placeholder = false;
   private float birth;
 
   public void Start() {
     birth = Time.time;
   }
   public void Update() {
-    if(Time.time - birth > lifespan) {
+    if(placeholder) {
+      GetComponent<Rigidbody>().isKinematic = true;
+      GetComponent<MeshRenderer>().enabled = false;
+      GetComponent<CapsuleCollider>().enabled = false;
+    } else {
+      GetComponent<Rigidbody>().isKinematic = false;
+      GetComponent<MeshRenderer>().enabled = true;
+      GetComponent<CapsuleCollider>().enabled = true;
+    }
+    if(Time.time - birth > lifespan && !placeholder) {
       Destroy(gameObject);
     }
   }
