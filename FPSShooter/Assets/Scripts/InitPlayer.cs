2558796a8040abbd80ba2f3b48ea224a9df4b492
Campbell Crowley <github@campbellcrowley.com>
2017-05-10 14:32:41 -0700

    Begin FPS Shooter project

---
+++ FPSShooter/Assets/Scripts/InitPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public
+class InitPlayer : MonoBehaviour {
+  // Height off the ground to spawn
+  [SerializeField] public float spawnHeight = 2;
+ private
+  bool spawned = false;
+ public
+  void go(float x, float y, float z) {
+    if (!spawned) {
+      transform.position = new Vector3(x, y + spawnHeight, z);
+      Debug.Log("Player Spawned\n" + transform.position);
+      spawned = true;
+    }
+  }
+ public
+  void updatePosition(float x, float y, float z) {
+    transform.position = new Vector3(x, y, z);
+    Debug.Log("Player moved to " + transform.position);
+  }
+}
